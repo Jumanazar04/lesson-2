4dc2b6c85401e03333627c15acc9b4895e2308da
Jumanazar04 <yusupovjumanazar004@gmail.com>
2023-09-30 16:00:13 +0500

    todo

--- app.js
+++ app.js
@@ -7,16 +7,44 @@ const formCreate = document.querySelector('.form-create')
 const inputCreate = document.querySelector('.input-create')
 const massegeCreate = document.querySelector('.massege-create')
 
+const todos = JSON.parse(localStorage.getItem('list')) 
+    ? JSON.parse(localStorage.getItem('list'))
+    : []
+ console.log(todos);
 
+// show massage
 
+function showMassage (where, massege){
+    document.querySelector(`${where}`).textContent = massege ;
+    setTimeout(()=>{
+        document.querySelector(`${where}`).textContent = ''
+    }, 3000)
+}
+
+// set todos 
+
+function setTodos() {
+    localStorage.setItem('list', JSON.stringify(todos))
+}
+
+// get todos 
+
+function getTodos(){
+    const getTodo = JSON.parse(localStorage.getItem('list'))
+    
+}
 
 formCreate.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const input = inputCreate.value
-    console.log(input);
-
-    if(inputCreate.value.trim() !== ''){
-        
-    }
+    const input = inputCreate.value.trim()
+   
+    if(input.length){
+        todos.push({ text: input, time: '30:09'})
+        setTodos()
+        getTodos()
+    }else {
+        showMassage( '.massege-create', 'Please, Enter some text...')
+    }   
+    formCreate.reset()
 })
