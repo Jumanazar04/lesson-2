1d3c6a96f442a3f01fa3ac64b1f865bcde7a5ae6
Jumanazar04 <yusupovjumanazar004@gmail.com>
2023-10-02 11:23:42 +0500

    t

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const secondEl = document.querySelector('.second')
 const formCreate = document.querySelector('.form-create')
 const inputCreate = document.querySelector('.input-create')
 const massegeCreate = document.querySelector('.massege-create')
+const listGroup = document.querySelector('list-group')
 
 const todos = JSON.parse(localStorage.getItem('list')) 
     ? JSON.parse(localStorage.getItem('list'))
@@ -31,7 +32,19 @@ function setTodos() {
 
 function getTodos(){
     const getTodo = JSON.parse(localStorage.getItem('list'))
-    
+    console.log(getTodo);
+    // getTodo.forEach( (item, i)=> {
+    //     listGroup.innerHTML = `
+    //     <li class="list-item">
+    //         <span class="todo-text">${item.text}</span>
+    //         <div class="elements">
+    //             <span class="time-elements">01.10</span>
+    //             <img width="25px" height="25px" src="./img/edit.svg" class="edit_img" alt="edit">
+    //             <img width="25px" height="25px" src="./img/delete.svg" class="delete-img" alt="delete">
+    //         </div>
+    //     </li>
+    //     `
+    // });
 }
 
 formCreate.addEventListener('submit', (e) => {
